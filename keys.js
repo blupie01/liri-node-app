102358e0110d158ca5fb41e73b2e52aeb900b51d
Jeff <Jeff>
2017-01-23 17:37:40 -0500

    pushing for backup

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: "hAdrefQl7JiFAlj4lwAVbHQGQ",
+    consumer_secret: "j6FluVxtIiqWfQVMiydt9zJKSuKPckAdEDOCLBUoFAofdwdgSK",
+    access_token_key: "823283624409788417-90CxTcwFaiCzQmjBTvOL6f1p57PazRa",
+    access_token_secret: "NMMSqHqLEMaPF3iHInqC3FBiChpzOgwja7luR7Nc9C7gB",
+};
